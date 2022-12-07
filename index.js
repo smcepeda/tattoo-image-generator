de6db23e00909ba6f7042d589a34ef7256787071
Santiago Cepeda <smcepeda@gmail.com>
2022-12-07 22:05:30 +0100

    Add design changes

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+const path = require("path");
+
 const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/openai", require("./routes/openaiRoutes"));
 
--- routes/openaiRoutes.js
+++ routes/openaiRoutes.js
@@ -2,11 +2,6 @@ const express = require("express");
 const { generateImage } = require("../controllers/openaiController");
 const router = express.Router();
 
-const app = express();
-// Enable body parser
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
 router.post("/generateimage", generateImage);
 
 module.exports = router;
